cfd294abc08cd6f4c03cd8be1b51b6f4d72c0b9d
Matthew Crnich <matthew.crnich@gmail.com>
2020-08-24 16:27:18 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,8 +1,14 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+      :patriach => {
       }, 
+      :matriarch => {
+      }
+      :hero => {
+      }
+      :hero_friends => {
+      }
     :capulet => {
       #your key/value pairs here!
       }}
